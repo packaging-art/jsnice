1a04b4e742ed56b512cf871e4c4bca589d6b581d
deadtype <yagnyuk@me.com>
2014-05-01 17:02:58 -0700

    Added correct js

--- js/global.js
+++ js/global.js
@@ -32,10 +32,14 @@ $.fn.toPx = function(settings){
     return Math.round(that * scopeVal);
 };
 
+//Set height of textareas
+
 function viewport() {
     var height = (window.innerHeight ? window.innerHeight : $w.height());
     var full = height - $(15).toPx({});
     $(".CodeMirror").css('height',full);
+    $(".intro_slide").css('height',full);
+
 }
 
 $(document).ready(function() {
@@ -46,3 +50,44 @@ $(document).ready(function() {
     });
 });
 
+//Remove intro_slide
+
+$( "#submit" ).click(function() {
+  $( "#intro_area" ).addClass( "hidden" );
+});
+
+//Social_Links
+		//Load Twitter
+	    window.twttr = (function (d,s,id) {
+	        var t, js, fjs = d.getElementsByTagName(s)[0];
+	        if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+	        js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+	        return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+	      }(document, "script", "twitter-wjs"));
+		//Load Facebook
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=456516547700866";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		//Load G+
+		(function() {
+			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			po.src = 'https://apis.google.com/js/platform.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+
+//Intialize CodeMirror for syntax highlighting and code editing
+
+	var editor = CodeMirror.fromTextArea(document.getElementById("inputjs"), {
+	    mode: "javascript",
+	    lineNumbers: true,
+	    lineWrapping: true
+	});
+	var editor = CodeMirror.fromTextArea(document.getElementById("outputjs"), {
+	    mode: "javascript",
+	    lineNumbers: true,
+	    lineWrapping: true
+	});
